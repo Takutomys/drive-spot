c26781e85bbb392fd981b1df4258770ddb8ee081
Takutomys <106647199+Takutomys@users.noreply.github.com>
2022-10-09 02:05:17 +0000

    ユーザー公開・非公開設定機能

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -11,9 +11,21 @@ class Public::EndUsersController < ApplicationController
   def update
     @end_user = EndUser.find(params[:id])
     @end_user.update(end_user_params)
-    redirect_to end_user_path
+    redirect_to end_user_path(current_end_user)
   end
-
+  
+  def release
+    @end_user = EndUser.find(params[:id])
+    @end_user.released! unmless @end_user.released?
+    ridirect_to edit_end_user_path(current_end_user), notice: 'このアカウントを公開しました'
+  end 
+  
+  def nonrelease
+    @end_user = EndUser.find(params[:id])
+    @end_user.nonreleased! unless @end_user.nonreleased?
+    ridirect_to edit_end_user_path(current_end_user), notice: 'このアカウントを非公開にしました'
+  end
+  
   def unsubscribe
   end
 
