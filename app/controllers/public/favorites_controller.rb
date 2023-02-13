e2a12a92aa5ab3dbac789f057a1ef882b0ade4ee
Takutomys <106647199+Takutomys@users.noreply.github.com>
2023-02-13 15:30:04 +0000

    [fix]layout

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,13 +3,13 @@ class Public::FavoritesController < ApplicationController
     tweet = Tweet.find(params[:tweet_id])
     favorite = current_end_user.favorites.new(tweet_id: tweet.id)
     favorite.save
-    redirect_to tweets_path
-  end 
-  
+    redirect_to tweet_path(tweet.id)
+  end
+
   def destroy
     tweet = Tweet.find(params[:tweet_id])
     favorite = current_end_user.favorites.find_by(tweet_id: tweet.id)
     favorite.destroy
-    redirect_to tweets_path
-  end 
+    redirect_to tweet_path(tweet.id)
+  end
 end
